8fffdae59236882e0ca66692d7da7ac52ed56b89
YIYUNCHEN <tpump729@gmail.com>
2018-05-17 22:01:37 +0800

    third commit

--- 歷屆4-1.c
+++ 歷屆4-1.c
@@ -4,12 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int j= 1,a=0;
-while(j <= 256){
-a++;
-j <<= 1;
+	int NumRer= 1,TimeS=0;
+while(NumRer <= 256){
+TimeS++;//加一 
+NumRer <<= 1;//往右推一位 
 }
-	printf("%d ",a);
+	printf("%d ",TimeS);
 	return 0;
 }
 
